4c269da5a268bc41947d5981f8aaaf04a378d264
MahshadOliaie <mahshadoliaie@gmail.com>
2024-02-19 23:53:58 +0330

    add heade rand searchc bar

---
+++ src/components/HomePage/HomePage.jsx
@@ -0,0 +1,14 @@
+import MyHeader from "../MyHeader/MyHeader"
+import SearchBar from "../SearchBar/SearchBar"
+
+
+function HomePage() {
+    return (
+        <>
+            <MyHeader />
+            <SearchBar />
+        </>
+    )
+}
+
+export default HomePage

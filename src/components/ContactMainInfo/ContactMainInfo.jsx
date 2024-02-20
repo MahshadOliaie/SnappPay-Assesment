b381420cc3782a8ab929c8e4ec6e5a16c238a1e8
MahshadOliaie <mahshadoliaie@gmail.com>
2024-02-21 01:27:07 +0330

    add main info component

--- src/components/ContactMainInfo/ContactMainInfo.jsx
+++ src/components/ContactMainInfo/ContactMainInfo.jsx
@@ -1,8 +1,24 @@
 import CSS from './contactMainInfo.module.css'
 
-function ContactMainInfo(){
-    return(
-        <></>
+function ContactMainInfo() {
+    return (
+        <>
+            <div className={CSS.mainInfo}>
+                <div className={CSS.image}>
+                    <img src="/public/user.jpeg" alt="" />
+                </div>
+                <div className={CSS.name}>
+                    <h3 className={CSS.nameTilte}>name</h3>
+                </div>
+                <div className={CSS.icons}>
+                    <svg className={CSS.messageIcon} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M160 368c26.5 0 48 21.5 48 48v16l72.5-54.4c8.3-6.2 18.4-9.6 28.8-9.6H448c8.8 0 16-7.2 16-16V64c0-8.8-7.2-16-16-16H64c-8.8 0-16 7.2-16 16V352c0 8.8 7.2 16 16 16h96zm48 124l-.2 .2-5.1 3.8-17.1 12.8c-4.8 3.6-11.3 4.2-16.8 1.5s-8.8-8.2-8.8-14.3V474.7v-6.4V468v-4V416H112 64c-35.3 0-64-28.7-64-64V64C0 28.7 28.7 0 64 0H448c35.3 0 64 28.7 64 64V352c0 35.3-28.7 64-64 64H309.3L208 492z" /></svg>
+                    <svg className={CSS.phoneIcon} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M164.9 24.6c-7.7-18.6-28-28.5-47.4-23.2l-88 24C12.1 30.2 0 46 0 64C0 311.4 200.6 512 448 512c18 0 33.8-12.1 38.6-29.5l24-88c5.3-19.4-4.6-39.7-23.2-47.4l-96-40c-16.3-6.8-35.2-2.1-46.3 11.6L304.7 368C234.3 334.7 177.3 277.7 144 207.3L193.3 167c13.7-11.2 18.4-30 11.6-46.3l-40-96z" /></svg>
+                    <svg className={CSS.videoCallIcon} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path d="M0 128C0 92.7 28.7 64 64 64H320c35.3 0 64 28.7 64 64V384c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V128zM559.1 99.8c10.4 5.6 16.9 16.4 16.9 28.2V384c0 11.8-6.5 22.6-16.9 28.2s-23 5-32.9-1.6l-96-64L416 337.1V320 192 174.9l14.2-9.5 96-64c9.8-6.5 22.4-7.2 32.9-1.6z" /></svg>
+                    <svg className={CSS.emailIcon} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M64 112c-8.8 0-16 7.2-16 16v22.1L220.5 291.7c20.7 17 50.4 17 71.1 0L464 150.1V128c0-8.8-7.2-16-16-16H64zM48 212.2V384c0 8.8 7.2 16 16 16H448c8.8 0 16-7.2 16-16V212.2L322 328.8c-38.4 31.5-93.7 31.5-132 0L48 212.2zM0 128C0 92.7 28.7 64 64 64H448c35.3 0 64 28.7 64 64V384c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V128z" /></svg>
+                </div>
+            </div>
+
+        </>
     )
 }
 
--- src/components/ContactMoreInfo/ContactMoreInfo.jsx
+++ src/components/ContactMoreInfo/ContactMoreInfo.jsx
@@ -1,8 +1,27 @@
 import CSS from './contactMoreInfo.module.css'
 
-function ContactMoreInfo(){
-    return(
-        <></>
+function ContactMoreInfo() {
+    return (
+        <>
+            <div className={CSS.moreInfo}>
+                <div className={CSS.phoneNumber}>
+                    <p className={CSS.title}>Mobile</p>
+                    <p className={CSS.titleInfo}>PhoneNumber</p>
+                </div>
+                <div className={CSS.email}>
+                    <p className={CSS.title}>Email</p>
+                    <p className={CSS.titleInfo}>Email</p>
+                </div>
+                <div className={CSS.address}>
+                    <p className={CSS.title}>Address</p>
+                    <p className={CSS.titleInfo}>Address</p>
+                </div>
+                <div className={CSS.birthday}>
+                    <p className={CSS.title}>Birthday</p>
+                    <p className={CSS.titleInfo}>Date</p>
+                </div>
+            </div>
+        </>
     )
 }
 export default ContactMoreInfo

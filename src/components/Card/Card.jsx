c6eadeadd1da6019e0125daf322d9424ed5bd3c1
MahshadOliaie <mahshadoliaie@gmail.com>
2024-02-21 12:14:26 +0330

    add hover

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,10 +1,12 @@
 import CSS from './card.module.css'
 
 function Card({ user }) {
-    const { name, location, phone , picture } = user
+    const { name, location, phone, picture } = user
     return (
         <div className={CSS.card}>
-            <img src={picture.large} alt="" className={CSS.image} />
+            <div className={CSS.image}>
+                <img src={picture.large} alt="" />
+            </div>
             <div className={CSS.about}>
                 <h3 className={CSS.name}>{`${name.first} ${name.last}`}</h3>
                 <p className={CSS.city}>{location.city}</p>
